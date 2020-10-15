d52d06f841b3e3cdfdb23dd5ef871c7c09c45970
Johnny Ho <jonnyho396@gmail.com>
2020-10-15 12:01:24 +1030

    seq max length to 20

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1158,7 +1158,7 @@ class BertForSequenceScoreTag(BertPreTrainedModel):
         # first_token_tensor = sequence_output[:, 0]
 
         # first_token_tensor = sequence_output.view(sequence_output.size(0)*sequence_output.size(1)*sequence_output.size(2),-1)
-        sequence_output = sequence_output[:,:,0:max_seq_len, :]
+        sequence_output = sequence_output[:,:,0:self.max_seq, :]
         first_token_tensor = sequence_output.contiguous().view(sequence_output.size(0),sequence_output.size(1), -1)
 
         pooled_output = self.pool(first_token_tensor)
