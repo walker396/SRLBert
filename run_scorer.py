0ebf2194e879131cfaa1307763b5085e22b2987d
Johnny Ho <jonnyho396@gmail.com>
2020-11-14 15:16:31 +1030

    debug tagging

--- run_scorer.py
+++ run_scorer.py
@@ -271,8 +271,8 @@ def transform_tag_features(features, tag_tokenizer, max_seq_length):
         token_tag_sequence_a = example.token_tag_sequence_a
         len_seq_a = example.len_seq_a
         token_tag_sequence_a.aspect_padding(max_num_aspect)
-        for temtag in token_tag_sequence_a:
-            print(temtag)
+        print(token_tag_sequence_a)
+
         tag_ids_list_a = token_tag_sequence_a.convert_to_ids(tag_tokenizer)
         input_tag_ids = []
         if example.token_tag_sequence_b != None:
