c18b638c29b9af5322b362f1ab3cb160f90acda3
Johnny Ho <jonnyho396@gmail.com>
2020-11-15 21:42:11 +1030

    debug tagging

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -703,6 +703,65 @@ class BertModel(BertPreTrainedModel):
         return encoded_layers, pooled_output
 
 
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class TransformerModel(nn.Module):
+
+    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        from torch.nn import TransformerEncoder, TransformerEncoderLayer
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp
+        self.decoder = nn.Linear(ninp, ntoken)
+
+        self.init_weights()
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, src_mask):
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.decoder(output)
+        return output
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
 class BertForPreTraining(BertPreTrainedModel):
     """BERT model with pre-training heads.
     This module comprises the BERT model followed by the two pre-training heads:
@@ -1049,7 +1108,20 @@ class BertForSequenceScoreTag(BertPreTrainedModel):
             self.tag_model = TagEmebedding(tag_config)
             self.dense = nn.Linear(tag_config.num_aspect * tag_config.hidden_size, tag_config.hidden_size)
             # Johnny
-            # self.dense = nn.Transformer(d_model=tag_config.num_aspect * tag_config.hidden_size, nhead=16, num_encoder_layers=12)
+            # >> > encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+            # >> > transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
+            # >> > src = torch.rand(10, 32, 512)
+            # >> > out = transformer_encoder(src)
+            #
+            #
+            #
+            # >> > transformer_model = nn.Transformer(nhead=16, num_encoder_layers=12)
+            # >> > src = torch.rand((10, 32, 512))
+            # >> > tgt = torch.rand((20, 32, 512))
+            # >> > out = transformer_model(src, tgt)
+            # self.dense = nn.Transformer(nhead=16, num_encoder_layers=12)
+            encoder_layer = nn.TransformerEncoderLayer(d_model=tag_config.num_aspect * tag_config.hidden_size, nhead=8,dim_feedforward=tag_config.hidden_size)
+            self.dense = nn.TransformerEncoder(encoder_layer, num_layers=6)
         else:
             hidden_size = config.hidden_size
         use_tag = True
