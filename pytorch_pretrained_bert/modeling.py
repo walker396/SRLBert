f7ac8b4d94cfca17670737deb2255c9d0a5ab03d
Johnny Ho <jonnyho396@gmail.com>
2020-11-15 21:57:49 +1030

    debug tagging

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1123,7 +1123,7 @@ class BertForSequenceScoreTag(BertPreTrainedModel):
             print(tag_config.num_aspect * tag_config.hidden_size)
             print(tag_config.hidden_size)
 
-            encoder_layer = nn.TransformerEncoderLayer(d_model=tag_config.num_aspect * tag_config.hidden_size, nhead=16,dim_feedforward=tag_config.hidden_size)
+            encoder_layer = nn.TransformerEncoderLayer(d_model=tag_config.num_aspect * tag_config.hidden_size, nhead=9,dim_feedforward=tag_config.hidden_size)
             self.dense = nn.TransformerEncoder(encoder_layer, num_layers=6)
         else:
             hidden_size = config.hidden_size
