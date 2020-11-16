6d60235a646c4446d3cd8c69e8e6db330b68d4b7
Johnny Ho <jonnyho396@gmail.com>
2020-11-16 19:10:21 +1030

    update input of transformer

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1123,7 +1123,8 @@ class BertForSequenceScoreTag(BertPreTrainedModel):
             print(tag_config.num_aspect * tag_config.hidden_size)
             print(tag_config.hidden_size)
 
-            encoder_layer = nn.TransformerEncoderLayer(d_model=tag_config.num_aspect * tag_config.hidden_size, nhead=9,dim_feedforward=tag_config.hidden_size)
+            # encoder_layer = nn.TransformerEncoderLayer(d_model=tag_config.num_aspect * tag_config.hidden_size, nhead=9,dim_feedforward=tag_config.hidden_size)
+            encoder_layer = nn.TransformerEncoderLayer(d_model=config.hidden_size + tag_config.hidden_size, nhead=9,dim_feedforward=config.hidden_size + tag_config.hidden_size)
             self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=6)
 
             self.dense1 = nn.Linear(tag_config.num_aspect * tag_config.hidden_size, tag_config.hidden_size)
